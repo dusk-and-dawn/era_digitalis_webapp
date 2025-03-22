29b87a470d9be37a49449635060053ba3139db8e
dusk-and-dawn <david.rotnicki@gmail.com>
2025-03-22 16:46:05 +0100

    update fuer josias

--- flask-backend.py
+++ flask-backend.py
@@ -1,5 +1,6 @@
 from flask import Flask, request, jsonify
 from flask_cors import CORS
+from josias_func import execute_program
 
 
 app = Flask(__name__)
@@ -8,6 +9,13 @@ CORS(app)
 @app.route('/api/ERA-AI', methods=['GET'])
 def test():
     return jsonify({'message':'Hi there'})
+
+@app.route('/josias_code', methods=['POST', 'GET'])
+def audio_transition():
+    # hier kommt dein Code rein Josias 
+    execute_program()
+    return  #json oder html datei 
+
 
 if __name__ == "__main__":
     app.run(port=5000)
---
+++ josias_func.py
@@ -0,0 +1,5 @@
+
+
+
+def execute_program():
+    pass
